4efd9fa6c3912d1ea4bfb8074e238d9aecda0ea2
Ananya Singh <as2863@njit,edu>
2023-05-12 23:59:26 -0400

    initial ChatGPT POC

--- ChatGPT.py
+++ ChatGPT.py
@@ -2,3 +2,15 @@
 This is POC
 '''
 
+import openai
+
+openai.api_key_path = ".openAiKey"
+
+completion = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    temperature=0.7,
+    max_tokens=2000,
+    messages=[{"role": "user", "content": "In what year did Spain win the World?"}]
+)
+
+print(completion.choices[0].message.content)
